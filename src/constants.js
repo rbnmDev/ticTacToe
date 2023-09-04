e3bd9bbd6699e5b9d9901aa2aa091d0d0e10ccf8
Ruben Moreno <134232975+rubenmmoreno@users.noreply.github.com>
2023-09-04 11:57:38 +0200

    backend init

--- src/constants.js
+++ src/constants.js
@@ -14,4 +14,60 @@ const WINNER_COMBOS = [
   [2, 4, 6],
 ]
 
+
+export const minimax = (board, depth, isMaximizing, player, opponent) => {
+  if (checkWinner(board, player)) {
+    return 10 - depth;
+  } else if (checkWinner(board, opponent)) {
+    return depth - 10;
+  } else if (checkTie(board)) {
+    return 0;
+  }
+
+  if (isMaximizing) {
+    let maxEval = -Infinity;
+    let bestMove = -1;
+
+    for (let i = 0; i < board.length; i++) {
+      if (board[i] === null) {
+        board[i] = player;
+        let evaluation = minimax(board, depth + 1, false, player, opponent);
+        board[i] = null;
+
+        if (evaluation > maxEval) {
+          maxEval = evaluation;
+          bestMove = i;
+        }
+      }
+    }
+
+    if (depth === 0) {
+      // Si estamos en el nivel superior, devuelve la mejor jugada
+      return bestMove;
+    }
+
+    return maxEval;
+  } else {
+    let minEval = Infinity;
+    let bestMove = -1;
+
+    for (let i = 0; i < board.length; i++) {
+      if (board[i] === null) {
+        board[i] = opponent;
+        let evaluation = minimax(board, depth + 1, true, player, opponent);
+        board[i] = null;
+
+        if (evaluation < minEval) {
+          minEval = evaluation;
+          bestMove = i;
+        }
+      }
+    }
+
+    return minEval;
+  }
+};
+
+
+
 export { TURNS, WINNER_COMBOS }
