0843c8c0218edb8caca81da6e01d1ab825642be9
Ruben Moreno <134232975+rubenmmoreno@users.noreply.github.com>
2023-09-06 08:46:26 +0200

    configuraciones nuevas

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,41 @@
+import { backend_url } from '../config.js';
+
+const Login = ({ redirect }) => {
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const userName = event.target.userName.value;
+        const password = event.target.password.value;
+        const user = { userName, password };
+        const response = await fetch(`${backend_url}/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        });
+        if (!response.ok) {
+            const message = `An error has FUCKING occured: ${response.status}`;
+            console.log(message);
+            return;
+        }
+        const data = await response.json();
+        console.log(data);
+        const token = data.token;
+        localStorage.setItem('token', token);
+        redirect();
+    }
+
+    return (
+        <section className="login">
+            <form className="login__form" onSubmit={handleSubmit}>
+                <label className="login__form__label" htmlFor="userName">user</label>
+                <input className="login__form__input" type="text" name="userName" id="userName" />
+                <label className="login__form__label" htmlFor="password">password</label>
+                <input className="login__form__input" type="password" name="password" id="password" />
+                <button className="login__form__button" type="submit">Iniciar sesión</button>
+            </form>
+        </section>
+    )
+}
+
+export default Login
